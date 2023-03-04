b2684cb22ebe849d76532d09f15fb3c852c30f01
geodevcode <geodevinstitute@gmail.com>
2023-03-04 23:16:38 +0100

    AllCountry.jsx was imported

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from 'components/NavBar'
+import AllCountry from './AllCountry'
 
 const Home = () => {
   return (
     <div>
       <NavBar/>
+      <AllCountry/>
     </div>
   )
 }
