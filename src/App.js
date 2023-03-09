14386034d8407972bad76e7fff176f19826e0405
geodevcode <geodevinstitute@gmail.com>
2023-03-09 15:55:08 +0100

    material ui theme provider added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Home from 'components/Home';
-import Country from 'components/Country';
-import AllCountry from 'components/AllCountry';
+import Home from "components/Home";
+import Country from "components/Country";
+// import AllCountry from "components/AllCountry";
+import { createMuiTheme, ThemeProvider } from "@mui/material";
 
+const theme = createMuiTheme({
+  palette: {
+    primary:{
+      main: "#2B3945",
+    } 
+  },
+  typography: {
+    fontFamily:"Nunito",
+    fontWeightRegular:400,
+    fontWeightMedium: 500
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/allcountry' element={<AllCountry/>}/>
-        <Route path='/allcountry/:id' element={<Country/>}/>
-      </Routes>
-      
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:id" element={<Country />} />
+          {/* <Route path="/allcountry" element={<AllCountry />} /> */}
+          {/* <Route path="/allcountry/:id" element={<Country />} /> */}
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
